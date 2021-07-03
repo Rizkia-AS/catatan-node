ad33b3b7584d110e3d4b00035353e2e2d969f8ca
Rizkia-AS <rizkia.as.ac@gmail.com>
2021-07-03 23:04:48 +0900

    belajar nodejs v9 menggunakan package npm

--- app.js
+++ app.js
@@ -1,73 +1,20 @@
-//  					NodeJS Core Modules
-
+//      NodeJS NPM
 /*
-belajar menggunakan module bawaan NodeJS
-1. selalu buka dokumentasi untuk melihat cara penggunaan https://nodejs.org/dist/latest-v14.x/docs/api/
+npm adalah sebuah tempat dimana para programer nodejs menyimpan modul modul mereka, dan kita dapat menggunakannya
+npmjs.com
+npm digunakan untuk menyimpan project oper source
 */ 
+console.log(`oke`);
 
-// A. file system
+const vtr = require(`validator`);
+const chalk = require(`chalk`);
+const scr = require(`./script.js`);
+// const mon = require(``)
 
-// - import
-const fs = require(`fs`);
-const { stdout } = require("process");
+// console.log(vtr);
 
+console.log(vtr.isEmail(`rizkia.as.ac@gmail.com`));
+console.log(vtr.isMobilePhone(`084548943521`, 'id-ID'));
+console.log(chalk.magenta(`andini afrianti`));
 
-// menuliskan string ke file async
-
-// fs.writeFile(`data/mesage.text`,`coba`, (e) =>{
-//     if(e) console.log(e);
-//     console.log(`sukses`)
-// })
-
-
-// membaca string ke file async
-
-// const buku =fs.readFileSync(`data/tesfile.txt`,(e,data) => {
-//     if(e) throw e;
-//     return data;
-// })
-
-// membaca string ke file sync
-
-let data = null;
-data = JSON.parse(fs.readFileSync(`data/contact.json`).toString());
-
-// B. Readline
-const readline = require(`readline`);
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: stdout,
-});
-
-rl.question(`Nama : `,(nama) =>{
-    rl.question(`Kelas : `, (kelas) => {
-
-        data.unshift(new daftarSw(nama, kelas));
-        if(data.length > 3) { data.pop()}
-
-        // write filesynch (menuliskan string ke file secara synchronous)
-        try {
-            fs.writeFileSync(`data/contact.json`, JSON.stringify(data));
-        } catch (e) { console.log(e); }
-
-        rl.close();
-    })
-})
-
-
-
-// C. OS
-const os = require(`os`);
-console.log(os.type()); 
-
-
-
-
-
-
-
-function daftarSw(nama,kelas) {
-    this.nama = nama,
-    this.kelas = kelas
-} 
+scr.helo(`dini`, 12);
